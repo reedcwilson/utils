216be71798d3cb163eb9bf13456fb3314d2ae40c
Reed Wilson <reedcwilson@gmail.com>
2015-11-05 15:23:56 -0700

    add runConfigurations to defaults

--- idea-defaults.py
+++ idea-defaults.py
@@ -3,6 +3,7 @@
 import xml.etree.cElementTree as ET
 import os
 import sys
+import shutil
 
 idea_dir = ''
 
@@ -22,6 +23,18 @@ if len(sys.argv) > 1:
 # don't give a directory and it assumes current dir contains idea subdir
 if len(sys.argv) == 1:
   idea_dir = get_idea_dir(os.path.dirname(os.path.realpath(__file__)))
+
+
+#===============================================================================
+# Run Configurations
+#===============================================================================
+
+run_configurations_dir = os.path.join(idea_dir, 'runConfigurations')
+clean_install_u = os.path.join(run_configurations_dir, 'clean_install__U.xml')
+if not os.path.exists(run_configurations_dir):
+  os.makedirs(run_configurations_dir)
+if not os.path.exists(clean_install_u):
+    shutil.copy(os.path.dirname(os.path.realpath(__file__)) + '/clean_install__U.xml', clean_install_u)
 
 
 #===============================================================================
