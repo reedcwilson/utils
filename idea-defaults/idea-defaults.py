2237ae2517ffde57dfae50373cec7501172d86de
Reed Wilson <rwilson@ancestry.com>
2018-05-14 14:16:17 -0600

    style: limit to 80 cols

--- idea-defaults/idea-defaults.py
+++ idea-defaults/idea-defaults.py
@@ -10,8 +10,14 @@ idea_dir = ''
 
 def get_idea_dir(root_dir):
     if os.path.exists(root_dir):
-        dirs = [os.path.join(root_dir, name) for name in os.listdir(root_dir)
-                if name == '.idea' and os.path.isdir(os.path.join(root_dir, name))]
+        dirs = [
+                os.path.join(root_dir, name)
+                for name in os.listdir(root_dir)
+                if (
+                    name == '.idea' and
+                    os.path.isdir(os.path.join(root_dir, name))
+                )
+        ]
         if not len(dirs) == 1:
             print("didn't find the idea directory")
             sys.exit(1)
@@ -33,17 +39,24 @@ if len(sys.argv) == 1:
 # =============================================================================
 
 install_dir = os.path.join(idea_dir, 'runConfigurations')
-config_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'runConfigurations')
+config_dir = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        'runConfigurations')
 # make the install directory if it doesn't exist
 if not os.path.exists(install_dir):
     os.makedirs(install_dir)
 # copy all of the saved configs to the install dir
-configs = [ c for c in os.listdir(config_dir) if os.path.isfile(os.path.join(config_dir, c)) ]
+configs = [
+    c for c in os.listdir(config_dir)
+    if os.path.isfile(os.path.join(config_dir, c))
+]
 for config in configs:
     filename = os.path.basename(config)
     install_file = os.path.join(install_dir, filename)
     if not os.path.exists(install_file):
-        shutil.copy(os.path.join(config_dir, config), os.path.join(install_dir, config))
+        shutil.copy(
+                os.path.join(config_dir, config),
+                os.path.join(install_dir, config))
 
 # =============================================================================
 # Workspace Defaults
@@ -53,7 +66,8 @@ workspace_file = "%s/workspace.xml" % (idea_dir)
 tree = ET.parse(workspace_file)
 
 # get all the window info objects and set the auto_hide attr to true
-window_infos = tree.getroot().findall('component[@name="ToolWindowManager"]/layout/window_info')
+xpath = 'component[@name="ToolWindowManager"]/layout/window_info'
+window_infos = tree.getroot().findall(xpath)
 for info in window_infos:
     info.set("auto_hide", 'true')
 
