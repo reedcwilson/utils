d94c57bb01a451cdc5d75aef88bd8ec34b481110
Reed Wilson <rwilson@ancestry.com>
2021-01-04 12:04:35 -0700

    add exercise calculator

---
+++ exercise/README.md
@@ -0,0 +1,5 @@
+# exercise activity calculator
+
+1. Download a csv of activities from [Garmin Connect](https://connect.garmin.com/modern/activities) and name the file activities.csv. You may need to scroll down in the UI until you have loaded all of the activities you wish to download because it currently only downloads what you have loaded.
+
+2. Run `python calculator.py` to get a list of the number of activities and the time spent doing them for each activity type.
---
+++ exercise/calculator.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+import csv
+
+
+def get_activities():
+    with open('activities.csv', 'r') as f:
+        _ = f.readline().strip()
+        return [list(csv.reader([line]))[0] for line in f.readlines()]
+
+
+def calculate_hours(activities):
+    hours = 0
+    for activity in activities:
+        h, m, s = activity[6].split(':')
+        hours += float(h) + (float(m)/60) + (float(s)/60/60)
+    return hours
+
+
+def report(activities):
+    hours = calculate_hours(activities)
+    print(f'{activities[0][0]}: {len(activities)} for {hours:.2f} hours')
+
+
+def main():
+    activities = get_activities()
+    mtb, virtual, cycling, running, skiing = [], [], [], [], []
+    for activity in activities:
+        name = activity[0].lower()
+        if 'mountain biking' in name:
+            mtb.append(activity)
+        if 'virtual cycling' in name:
+            virtual.append(activity)
+        if 'indoor cycling' in name:
+            virtual.append(activity)
+        if 'cycling' == name:  # must equal - there are other types of cycling
+            cycling.append(activity)
+        if 'running' in name:
+            running.append(activity)
+        if 'skiing' in name:
+            skiing.append(activity)
+    report(mtb)
+    report(virtual)
+    report(cycling)
+    report(running)
+    report(skiing)
+
+
+if __name__ == '__main__':
+    main()
